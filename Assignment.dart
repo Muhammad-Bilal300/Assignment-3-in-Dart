23c012cc9fff3f7f8f11043f19038b20e311b2a8
Muhammad-Bilal300 <bilalaarbi300@gmail.com>
2021-10-03 23:56:05 +0500

    Assignment-3

--- Assignment.dart
+++ Assignment.dart
@@ -1,7 +1,6 @@
 void main(List<String> args) {
   var a = [2, 4, 6, 2, 3, 8, 4, 6, 10, 2];
-  // for (var i = 0; i < a.length; i++) {
-  //   for
+
   // }
 
   a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
@@ -29,19 +28,40 @@ void main(List<String> args) {
   } else {
     print("${number} is not a Prime Number.");
   }
-  
 
 //// -----------------------------------------------------------//
-
 
   var num = 7;
   for (var i = 1; i <= 15; i++) {
     print("${num} x ${i} = ${num * i}");
   }
 
-
 // -----------------------------------------------------------//
 
+  var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+  for (var i in fruits) {
+    print(i);
+  }
 
-  
+  // -----------------------------------------------------------//
+
+  for (var i = 5; i <= 100; i += 5) {
+    print(i);
+  }
+
+  // -----------------------------------------------------------//
+
+  var temp = 30;
+  var degreeChanger = ((temp * (9 / 5)) + 32);
+  print("${temp}oC is ${degreeChanger}oF");
+
+  temp = 86;
+  degreeChanger = ((5 / 9)*(temp - 32));
+  print("${temp}oF is ${degreeChanger}oC");
+
+  // -----------------------------------------------------------//
+
+
+
+
 }
