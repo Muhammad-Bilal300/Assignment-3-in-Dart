82af1426dd5489fac9a14e62684b21846bd6ded3
Muhammad-Bilal300 <bilalaarbi300@gmail.com>
2021-10-04 00:15:17 +0500

    Assignment-3

--- Assignment.dart
+++ Assignment.dart
@@ -1,9 +1,16 @@
 void main(List<String> args) {
-  var a = [2, 4, 6, 2, 3, 8, 4, 6, 10, 2];
+  
+  
+  var lst1 = [1, 2, 3, 4, 5, 6, 7];
+  var lst2 = [3, 5, 6, 7, 9, 10];
 
-  // }
+  print(lst1.where((e) => !lst2.contains(e)).toList());
 
-  a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+
+// ---------------------------------------------------------
+
+
+  var a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
   print(a);
   var evenNumbers = [];
   for (var i in a) {
@@ -56,12 +63,39 @@ void main(List<String> args) {
   print("${temp}oC is ${degreeChanger}oF");
 
   temp = 86;
-  degreeChanger = ((5 / 9)*(temp - 32));
+  degreeChanger = ((5 / 9) * (temp - 32));
   print("${temp}oF is ${degreeChanger}oC");
 
   // -----------------------------------------------------------//
 
+  var firstNum = 10;
+  var secondNum = 20;
+
+  var operator = "+";
+
+  if (operator == "+") {
+    print("${firstNum}+${secondNum} = ${firstNum + secondNum}");
+  } else if (operator == "-") {
+    print("${firstNum}+${secondNum} = ${firstNum - secondNum}");
+  } else if (operator == "*") {
+    print("${firstNum}+${secondNum} = ${firstNum * secondNum}");
+  } else if (operator == "/") {
+    print("${firstNum}+${secondNum} = ${firstNum / secondNum}");
+  } else if (operator == "%") {
+    print("${firstNum}+${secondNum} = ${firstNum % secondNum}");
+  }
 
 
 
+  // -------------------------------------------------------------------
+
+
+
+
+  var char = "a";
+  if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+    print("true");
+  } else {
+    print("False");
+  }
 }
